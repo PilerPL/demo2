9a72722ed51827ba519ec0535bbe922f06a8a270
Lenovo <Lenovo@163>
2020-03-18 20:58:54 +0800

    test

---
+++ src/main/java/com/yang/demo/DemoApplication.java
@@ -0,0 +1,39 @@
+package com.yang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(DemoApplication.class, args);
+        int[] a = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(getMaxArea(a));
+
+    }
+
+    public static int getMaxArea(int[] a) {
+        int hg = a.length;//数组长度
+        int left = 0;
+        int right = hg - 1;
+        //寻找最大面积
+        int x = right - left + 1;//长方形的长度
+        int y = Math.min(a[left], a[right]);
+        int area = x * y;
+        while (left != right) {
+            if (a[right] >= a[left]) {//左边大于右边 每次要移动高度比较小的那个 才有可能让面积增大
+                left = left + 1;
+            } else {
+                right = right - 1;
+            }
+            x = right - left;
+            y = Math.min(a[left], a[right]);
+            int temp = x * y;
+            if (temp > area) {
+                area = temp;
+            }
+        }
+        return area;
+    }
+}
